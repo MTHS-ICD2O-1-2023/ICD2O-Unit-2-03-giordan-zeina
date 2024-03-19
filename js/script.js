f2bef58a106411ee3f3f488bdef0e9355b7e5864
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-03-19 18:46:10 +0000

    dff

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Giordan Zeina
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
 
 function enterClicked() {
   // input
-  const firstName = document.getElementById("street-number").value
-  const userAge = parseInt(document.getElementById("street-name").value)
-
+  const streetNumber = parseInt(document.getElementById("street-number").value)
+  const streetName = (document.getElementById("street-name").value)
   // output
   document.getElementById("user-info").innerHTML =
-    "Your info is: " + firstName + ", age " + userAge + "."
+    "Your address is: " + streetNumber + ", streetName."
 }
