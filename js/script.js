53a82b60c33c7891915e211d86073e0e360031c8
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-03-19 18:39:01 +0000

    fkhbfxzbmnvx;lhmdblkmbbjnbc

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function enterClicked() {
+  // input
+  const firstName = document.getElementById("street-number").value
+  const userAge = parseInt(document.getElementById("street-name").value)
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your info is: " + firstName + ", age " + userAge + "."
+}
