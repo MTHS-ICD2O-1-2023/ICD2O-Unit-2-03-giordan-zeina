e4c8ae75e54d9768e98d2101ad86efdff763e168
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-03-21 18:04:37 +0000

    com

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@
 function enterClicked() {
   // input
   const streetNumber = document.getElementById("street-number").value
-  const streetName = parseInt(document.getElementById("street-name").value)
+  const streetName = (document.getElementById("street-name").value)
   // output
   document.getElementById("user-info").innerHTML =
-    "Your address is: " + streetNumber + ", streetName."
+    "Your address is: " + streetNumber + streetName.
 }
